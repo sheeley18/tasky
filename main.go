b70667404c7fc5216ff8f1aba2fbc1abaed81245
Sean Heeley <67065230+sheeley18@users.noreply.github.com>
2025-08-13 12:52:17 -0400

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"net/http"
-	"time"  // Add this import
+	"time"
 	controller "github.com/jeffthorne/tasky/controllers"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -12,7 +12,6 @@ func index(c *gin.Context) {
 	c.HTML(http.StatusOK, "login.html", nil)
 }
 
-// NEW: Add this function
 func healthInfo(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"status": "healthy",
@@ -31,26 +30,16 @@ func main() {
 	router.Static("/assets", "./assets")
 
 	router.GET("/", index)
-	router.GET("/health-info", healthInfo)  // NEW: Add this line
+	router.GET("/health-info", healthInfo)
 	router.GET("/todos/:userid", controller.GetTodos)
 	router.GET("/todo/:id", controller.GetTodo)
 	router.POST("/todo/:userid", controller.AddTodo)
 	router.DELETE("/todo/:userid/:id", controller.DeleteTodo)
 	router.DELETE("/todos/:userid", controller.ClearAll)
 	router.PUT("/todo", controller.UpdateTodo)
-
 	router.POST("/signup", controller.SignUp)
 	router.POST("/login", controller.Login)
 	router.GET("/todo", controller.Todo)
 
 	router.Run(":8080")
-}
-
-
-	router.POST("/signup", controller.SignUp)
-	router.POST("/login", controller.Login)
-	router.GET("/todo", controller.Todo)
-
-	router.Run(":8080" )
-
 }
